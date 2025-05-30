9ce7341d8662c18309c7c88fc7ac5c954ff7d881
Alex_H <ewebotah@gmail.com>
2025-05-30 06:01:29 +0200

    started protect

---
+++ src/main/java/com/example/itfaq/ItfaqApplication.java
@@ -0,0 +1,13 @@
+package com.example.itfaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItfaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItfaqApplication.class, args);
+	}
+
+}
